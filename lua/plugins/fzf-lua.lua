e18d9649ff02f7ac093c06495007b9320cfb017b
Ean244 <ianlim224@gmail.com>
2026-09-14 16:09:54 -0400

    Initial commit of LazyVim config

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      grep = {
+        rg_opts = "--column --line-number --no-heading --color=always --ignore-case --max-columns=4096 -e",
+      },
+    },
+  },
+}
